6dc56befc57e70405d896f7575f5320bde4123d2
Elleanna Dwyer <edwyer@tonicdesign.com>
2017-06-19 09:47:40 -0400

    Removed initialize and pageContextManager.

--- app/initialize.js
+++
@@ -1,6 +0,0 @@
-import { PageContextManager } from 'scripts/pageContextManager.js'
-
-document.addEventListener('DOMContentLoaded', () => {
-  tonic.contextManager = new PageContextManager();
-  tonic.contextManager.initialize($('#page-container').data('page-type'));
-});
